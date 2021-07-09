44856cfa1e99136c68a8318ebc33a5385284d8bc
Gloria Buriticá <gloriaburit@gmail.com>
2021-07-09 10:08:17 +0200

    ranks
    rank and ranktransform functions with documentation.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Auxiliar_functions
- Read 1st
+ 
+ ## Marginal transformation to Fréchet domain of attraction. 
+ ## Ranks transform  : Given a trajectory of observations:
+ ##                      ranks              -> returns the vector of ranks
+ ##                      ranktranform  -> returns the vector with rank transform applied
+
+Examples of implementation: 
+
+ sample  <- arima.sim(n = 5000, list(ar=0.5, ma=0.3), rand.gen=function(n) rt(n,df=1))
+ plot.ts(abs(sample))
+ plot.ts(ranktransform(sample2))
+ ranktransform(c(NA,NA,3,5,5))
+
+
+General comments: 
+You might want to handle repeated values and NA's before applying the functions.
+
+Comments to each function: 
+
+ranks: NA values are transformed to 0 and then sorted in order of appearence.
+ranktranform: NA and 0's set to 0. 
---
+++ rank_transform.R
@@ -0,0 +1,23 @@
+#######################################################################
+#######################################################################
+#######################################################################
+#######################################################################
+#### Gloria Buriticá
+#######################################################################
+r             <- function(x0){
+  v0 <- abs(vector(length = length(x0), mode = "integer"))
+  y0 <- order(x0, decreasing=TRUE)
+  for(i in 1:length(x0))  v0[y0[i]] = i 
+  return(v0)
+}
+ranktransform <- function(x0){
+    samplerank0           <- (length(x0)+1)/r(x0)
+    samplerank0[x0 == 0]  <-  0                ## zeros in the sample set to zero.
+    if(sum(is.na(x0))> 0){
+      samplerank0[ is.na(x0)] <- 0             ## I'm setting the NA's to 0.
+      print("NA's were found and set to 0")
+    }
+    return(samplerank0)
+}
+
+
